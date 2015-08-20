bf8f9759be2abe0cc05e5656b8e2da9db023cb6d
DaiJia <daijia1009@gmail.com>
2015-08-20 15:50:30 +0800

    add comment

--- src/learn_clojure_project/Hello.java
+++ src/learn_clojure_project/Hello.java
@@ -2,6 +2,10 @@ package learn_clojure_project;
 
 public class Hello {
 	
+	/**
+	 * Add comment
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello Clojure");
 	}
