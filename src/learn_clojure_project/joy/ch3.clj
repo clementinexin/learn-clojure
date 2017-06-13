5df751f1d5de520a5e4e17c51996e504641fbe4d
daijiajia <daijiajia@meituan.com>
2017-06-13 09:39:42 +0800

    sicp.ch1

--- src/learn_clojure_project/joy/ch3.clj
+++ src/learn_clojure_project/joy/ch3.clj
@@ -73,4 +73,49 @@ eveil-false
 (print-last-name guys-whole-name-map)
 
 
+(for [x (range 2) y (range 2)] [x y (bit-xor x y)])
+
+
+(defn xors [max-x max-y]
+  (for [x (range max-x) y (range max-y)] [x y (bit-xor x y)]))
+(xors 2 2)
+
+
+
+
+(for [method (seq (.getMethods java.awt.Frame))
+      :let [method-name (.getName method)]
+      :when (re-find #"Vis" method-name)]
+  method-name)
+
+(.isVisible frame)
+(def frame (java.awt.Frame.))
+(.setVisible frame true)
+
+(.setSize frame (java.awt.Dimension. 200 200))
+
+(def gfx (.getGraphics frame))
+
+(.fillRect gfx 100 100 50 75)
+(.setColor gfx (java.awt.Color. 255 128 0))
+
+
+(doseq [[x y xor] (xors 200 200)]
+  (.setColor gfx (java.awt.Color xor xor xor))
+  (.fillRect gfx x y 1 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/learn_clojure_project/sicp/ch1.clj
+++ src/learn_clojure_project/sicp/ch1.clj
@@ -62,3 +62,82 @@
     (help 1.0)))
 (sqrt 3)
 
+(defn square [x]
+  (* x x))
+
+(defn double [x]
+  (+ x x))
+(defn square [x]
+  (exp (double (log x))))
+(square 3)
+
+(defn factorial [n]
+  (if (= n 1)
+    1
+    (* n (factorial (- n 1)))))
+(factorial 10)
+
+
+
+(defn factorial-iter [result count max]
+  (if (> count max)
+    result
+    (factorial-iter (* result count)
+                    (+ count 1)
+                    max)))
+(defn factorial [n]
+  (factorial-iter 1 1 n)
+  )
+
+(factorial 10)
+
+
+(defn fib [n]
+  (cond (= n 0) 0
+        (= n 1) 1
+        :else (+
+                (fib (- n 1))
+                (fib (- n 2))
+                )))
+
+(fib 3)
+
+(defn fib [n]
+  (fib-iter 1 0 n))
+
+(defn fib-iter [n2 n1 count]
+  (if (= count 0) 0
+                  (fib-iter (+ n2 n1)
+                            n2
+                            (- count 1))))
+
+(fib 3)
+
+
+(defn cube [x]
+  (* x x x))
+
+(defn p [x]
+  (- (* 3 x) (* 4 (cube x))))
+
+(defn abs [x]
+  (if (> x 0) x
+              (- 0 x)))
+
+
+(defn sin [angle]
+  (if (not (> (abs angle) 0.1)) angle
+                                (p (sin (/ angle 3.0)))))
+
+(sin 30)
+
+(defn expt [n]
+  (if (= n 0) 1
+    (* n (expt (- n 1)) )))
+(expt 3)
+
+(defn expt [n]
+  (expt-iter 1 0 n))
+
+(defn expt-iter)
+
